27031f1b2b1f8fcbc8f7ec3dee0112c7ed39ca06
z7pz <sdfygamer@gmail.com>
2024-08-28 12:58:32 +0300

    test

--- src/giveaway/giveaway.rs
+++ src/giveaway/giveaway.rs
@@ -3,7 +3,6 @@ use prisma_client::db::giveaway;
 use rand::seq::SliceRandom;
 use serenity::EditMessage;
 
-
 use super::options::{EndMessage, GiveawayOptions, StartMessage};
 use crate::{entities::*, prelude::*};
 
@@ -16,7 +15,6 @@ pub struct Giveaway {
     pub is_ended: bool,
 }
 
-
 impl Giveaway {
     pub fn new(message_id: MessageId, options: GiveawayOptions) -> Self {
         Self {
@@ -27,27 +25,51 @@ impl Giveaway {
             is_ended: false,
         }
     }
-    pub async fn  save(&self) -> Result<giveaway::Data, Error> {
+    pub async fn save(&self) -> Result<giveaway::Data, Error> {
         Ok(self.entity.create(&self).await?)
     }
-    pub async fn add_entry(&mut self, user_id: UserId, cache_http: impl CacheHttp) -> Result<Message, Error> {
+    pub async fn add_entry(
+        &mut self,
+        user_id: UserId,
+        cache_http: impl CacheHttp,
+    ) -> Result<Message, Error> {
         println!("Adding entry: {}", user_id);
         if self.is_ended {
             return Err("Giveaway has ended".into());
         }
         self.entries.push(user_id);
         println!("Added entry: {}", user_id);
-        Ok(self.update_message(cache_http, StartMessage::edit_message(&self.options, &self.entries)).await?)
+        Ok(self
+            .update_message(
+                cache_http,
+                StartMessage::edit_message(&self.options, &self.entries),
+            )
+            .await?)
     }
-    pub async fn update_message(&self, cache_http: impl CacheHttp, edit_message: EditMessage) -> Result<Message, Error> {
-        Ok(self.options.channel_id.edit_message(cache_http, self.message_id, edit_message).await?)
+    pub async fn update_message(
+        &self,
+        cache_http: impl CacheHttp,
+        edit_message: EditMessage,
+    ) -> Result<Message, Error> {
+        Ok(self
+            .options
+            .channel_id
+            .edit_message(cache_http, self.message_id, edit_message)
+            .await?)
     }
     pub async fn end(&mut self, cache_http: impl CacheHttp) -> Result<Message, Error> {
         self.is_ended = true;
         // TODO send a new message with the winners
-        Ok(self.update_message(cache_http, EndMessage::edit_message(&self.options, &self.entries, self.get_winners())).await?)
+        Ok(self
+            .update_message(
+                cache_http,
+                EndMessage::edit_message(&self.options, &self.entries, self.get_winners()),
+            )
+            .await?)
     }
     pub fn get_winners(&self) -> Vec<&UserId> {
-        self.entries.choose_multiple(&mut rand::thread_rng(), self.options.winners as usize).collect::<Vec<&UserId>>()
+        self.entries
+            .choose_multiple(&mut rand::thread_rng(), self.options.winners as usize)
+            .collect::<Vec<&UserId>>()
     }
 }
